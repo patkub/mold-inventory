6cd278484dc9e7b10b6bb8c5dbbc32054ac86e2e
Patrick Kubiak <epicpatka@gmail.com>
2026-02-23 17:17:59 -0500

    hono add jwk verification options

--- apps/mold-inventory-api/worker/middleware/jwt.ts
+++ apps/mold-inventory-api/worker/middleware/jwt.ts
@@ -10,6 +10,10 @@ const setupJWT = createMiddleware(async (c, next) => {
   const middleware = jwk({
     jwks_uri: (c) => `https://${c.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
     alg: ['RS256'],
+    verification: {
+      iss: `https://${c.env.AUTH0_DOMAIN}/`,
+      aud: 'https://mold-inventory-api-production.epicpatka.workers.dev/api'
+    }
   })
   return middleware(c, next)
 })
