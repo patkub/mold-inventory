152dfb11ec29a6d051049dab11ec9da0b1e796d6
Patrick Kubiak <epicpatka@gmail.com>
2026-04-03 08:05:42 -0400

    Migrate to ESLint 10

--- apps/mold-inventory-app/eslint.config.mjs
+++ apps/mold-inventory-app/eslint.config.mjs
@@ -1,15 +1,29 @@
 // eslint.config.mjs
+import globals from "globals";
 import { globalIgnores } from 'eslint/config'
 import js from '@eslint/js'
-// import next from 'eslint-config-next'
-import nextVitals from 'eslint-config-next/core-web-vitals'
+import next from '@next/eslint-plugin-next'
 import prettier from 'eslint-config-prettier'
 
 const eslintConfig = [
+  // Node/CommonJS globals
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.commonjs,
+      },
+    },
+  },
+
+  // JavaScript rules
   js.configs.recommended,
-  ...nextVitals,
+  // Next.js rules
+  next.configs.recommended,
+  // Turn off rules that might conflict with Prettier (must be last)
   prettier,
-  // Override default ignores of eslint-config-next.
+  
+  // Override default ignores of @next/eslint-plugin-next.
   globalIgnores([
     '.next/**',
     '.open-next/**',
